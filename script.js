2043b0a3e82a1543f22884dec74c5a20a222b325
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-02-15 16:01:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // PORTFOLIO
-document.getElementById(".img-port1").addEventListener("click", function() {
+document.getElementById("#img-port1").addEventListener("click", function() {
     // Substitua a URL abaixo pela URL para a qual você deseja redirecionar
     window.location.href = "projetos\portfolio1\portfolio1.html";
   });
