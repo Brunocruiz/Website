3a79952e9ceb22faab816952ca0f505c5b3ad733
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-02-15 17:05:11 -0300

    add texto digitado

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ document.getElementById("img-port1").addEventListener("click", function() {
     // Substitua a URL abaixo pela URL para a qual você deseja redirecionar
     window.location.href = "projetos/portfolio1/portfolio1.html";
   });
+
+  // TEXTO DIGITADO
+  document.addEventListener('DOMContentLoaded', function() {
+    // Texto que será animado
+    const texto = '<script>\nconsole.log("Bruno Martins da Cruz");\n</script>>';
+
+  
+    // Elemento onde o texto será exibido
+    const elementoTexto = document.getElementById('texto-animado');
+  
+    // Inicializa a animação do texto
+    typeWriter(texto, elementoTexto);
+  });
+  
+  // Função para animação de digitação
+  function typeWriter(text, targetElement) {
+    let i = 0;
+  
+    function type() {
+      if (i < text.length) {
+        targetElement.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, 50); // Ajuste aqui para controlar a velocidade de digitação
+      }
+    }
+  
+    type();
+  }
