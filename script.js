8c992a11946671358baea4cca7fb66f76bbec248
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-02-15 23:32:11 -0300

    95% completo

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// NAV ROLAGEM SUAVE
+document.addEventListener("DOMContentLoaded", function () {
+  // Adiciona o evento de clique a todos os links da navegação
+  document.querySelectorAll('.menu-desktop a').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          // Obtém o destino da rolagem
+          const targetId = this.getAttribute('href').substring(1);
+          const targetElement = document.getElementById(targetId);
+
+          // Rola suavemente até o destino
+          if (targetElement) {
+              window.scrollTo({
+                  top: targetElement.offsetTop,
+                  behavior: 'smooth'
+              });
+          }
+      });
+  });
+});
+
+
 // PORTFOLIO
 document.getElementById("img-port1").addEventListener("click", function() {
     // Substitua a URL abaixo pela URL para a qual você deseja redirecionar
