f89ea79eed05bb9c77796a28b465313ca9ca83e8
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-03-02 02:33:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,117 @@
-// NAV ROLAGEM SUAVE
-document.addEventListener("DOMContentLoaded", function () {
-  // Adiciona o evento de clique a todos os links da navegação
-  document.querySelectorAll('.nav-list a').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-          e.preventDefault();
-
-          // Obtém o destino da rolagem
-          const targetId = this.getAttribute('href').substring(1);
-          const targetElement = document.getElementById(targetId);
-
-          // Rola suavemente até o destino
-          if (targetElement) {
-              window.scrollTo({
-                  top: targetElement.offsetTop,
-                  behavior: 'smooth'
-              });
-          }
-      });
-  });
-});
-
-
-// DARK MOD
-
-const darkMode = document.querySelector('.dark-mode');
-
-darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode-variables');
-    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-})
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+  
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+  
+  /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+  
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+  
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+  
+    } else {
+  
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+  
+    }
+  }
+  
+  
+  /* ----- TYPING EFFECT ----- */
+  var typingEffect = new Typed(".typedText",{
+    strings : ["FullStack Developer.", "from Brazil."],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+  })
+  
+  
+  /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+  const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+  })
+  
+  /* -- HOME -- */
+  sr.reveal('.featured-text-card',{})
+  sr.reveal('.featured-name',{delay: 100})
+  sr.reveal('.featured-text-info',{delay: 200})
+  sr.reveal('.featured-text-btn',{delay: 200})
+  sr.reveal('.social_icons',{delay: 200})
+  sr.reveal('.featured-image',{delay: 300})
+  
+  
+  /* -- PROJECT BOX -- */
+  sr.reveal('.project-box',{interval: 200})
+  
+  /* -- HEADINGS -- */
+  sr.reveal('.top-header',{})
+  
+  /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+  
+  /* -- ABOUT INFO & CONTACT INFO -- */
+  const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+  })
+  
+  srLeft.reveal('.about-info',{delay: 100})
+  srLeft.reveal('.contact-info',{delay: 100})
+  
+  /* -- ABOUT SKILLS & FORM BOX -- */
+  const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+  })
+  
+  srRight.reveal('.skills-box',{delay: 100})
+  srRight.reveal('.form-control',{delay: 100})
+  
+  
+  
+  /* ----- CHANGE ACTIVE LINK ----- */
+  
+  const sections = document.querySelectorAll('section[id]')
+  
+  function scrollActive() {
+  const scrollY = window.scrollY;
+  
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
+  
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+  
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+  
+    }  else {
+  
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+  
+    }
+  })
+  }
+  
+  window.addEventListener('scroll', scrollActive)
